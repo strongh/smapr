fecae7b7f63c273970e9797557d279a3be424c94
Homer Strong <homer.strong@gmail.com>
2016-01-11 16:28:26 -0800

    first pass at fancy 3d globe

--- R/shiny_smap.r
+++ R/shiny_smap.r
@@ -10,6 +10,7 @@
 shiny_smap <- function() {
   require(shiny)
   require(ggplot2)
+  require(threejs)
   shinyApp(
     ui = fluidPage(
       titlePanel("SMAP Viewer"),
@@ -18,21 +19,39 @@ shiny_smap <- function() {
         sidebarPanel(
           helpText("Looking at SMAP soil moisture"),
 
-          dateInput("date", label = h3("Date input"), value = "2015-04-24")
+          dateInput("date", label = h3("Date input"), value = "2015-06-17")
         ),
 
         mainPanel(
           tabsetPanel(type = "tabs",
                       tabPanel("SMAP map",
-                               plotOutput("map"))
+                               plotOutput("map")),
+                      tabPanel("SMAP Globe",
+                               globeOutput("globe"))
 
           )   )
       )
     ),
     server = function(input, output) {
       output$map <- renderPlot({
-        ggplot(read.smap.l3(input$date), aes(lon, lat)) + geom_raster(aes(fill=soil.moisture))
+        ggplot(read.smap.l3(input$date, dataset.id = "SM_P"), aes(lon, lat)) + geom_raster(aes(fill=soil.moisture))
+      })
 
+      output$globe <- renderGlobe({
+        df <- read.smap.l3(input$date, dataset.id = "SM_P", reproject = FALSE)
+
+        df$q <- as.numeric(
+          cut(df$soil.moisture,
+              breaks=quantile(df$soil.moisture, probs=c(0,0.90,0.95,0.99,1)),
+              include.lowest=TRUE))
+        col = c("#0055ff","#00aaff","#00ffaa","#aaff00")[df$q]
+
+        globejs(lat = df$lat,
+                lon = df$lon,
+                atmosphere = TRUE,
+                pointsize = 1,
+                color = col,
+                val =-5 * log(df$soil.moisture))
       })
     }
   )
